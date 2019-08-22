58accb541dfccbf810078340d2e5cf9be05ab4ce
Aidan Beery <aidanb1049@gmail.com>
2019-08-22 10:58:38 -0700

    Generation of varying icons for different AQI levels

--- src/main/java/AirQualityMap/KMLgen.java
+++ src/main/java/AirQualityMap/KMLgen.java
@@ -5,8 +5,23 @@ import de.micromata.opengis.kml.v_2_2_0.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.EnumSet;
 
 public class KMLgen {
+    private enum AqiIcons{
+
+        Good("http://files.airnowtech.org/airnow/today/aqi0circle.png"),
+        Moderate("http://files.airnowtech.org/airnow/today/aqi1circle.png"),
+        Sensitive("http://files.airnowtech.org/airnow/today/aqi2circle.png"),
+        Unhealthy("http://files.airnowtech.org/airnow/today/aqi3circle.png"),
+        Very_Unhealthy("http://files.airnowtech.org/airnow/today/aqi4circle.png");
+
+
+        public final String href;
+        AqiIcons(String href){
+            this.href = href;
+        }
+    }
     private ArrayList<SensorData> sensorDataList;
     final Kml kml = new Kml();
     private Document doc;
@@ -28,23 +43,32 @@ public class KMLgen {
     }
 
     private void makeDataPoint(SensorData sensorData){
-
+        /*String styleUrl;
+        if(sensorData.getPM2_5() <= 50){
+            styleUrl = ("#Good");
+        }
+        else if((sensorData.getPM2_5() > 50) && (sensorData.getPM2_5() <= 100)){
+            styleUrl = ("#Moderate");
+        }
+        else if((sensorData.getPM2_5() > 100) && (sensorData.getPM2_5() <=150)){
+            styleUrl = ("#Sensitive");
+        }*/
         Placemark placemark = folder.createAndAddPlacemark();
         placemark.withName(sensorData.getName())
-                .withStyleUrl("#good")
+                .withStyleUrl("temporary string")
                 .withDescription("value = " + sensorData.getPM2_5())
                 .createAndSetLookAt().withLongitude(sensorData.getLon()).withLatitude(sensorData.getLat()).withAltitude(0).withRange(1200);
         placemark.createAndSetPoint().addToCoordinates(sensorData.getLon(), sensorData.getLat()).setAltitudeMode(AltitudeMode.RELATIVE_TO_GROUND);
-
     }
 
     private void makeStyleIcons(){
-        Icon goodIcon = new Icon().withHref("http://files.airnowtech.org/airnow/today/aqi0circle.png");
-        BalloonStyle goodBalloon = new BalloonStyle().withBgColor("ffffffff");
-        Style goodStyle = doc.createAndAddStyle();
-        goodStyle.withId("good").createAndSetIconStyle().withScale(1.0).withIcon(goodIcon);
-        goodStyle.createAndSetLabelStyle().withColor("ff43b3ff").withScale(1.2);
-        goodStyle.createAndSetBalloonStyle().withBgColor("ffffffff").withDisplayMode(DisplayMode.DEFAULT);
-
+        EnumSet<AqiIcons> styleList = EnumSet.allOf(AqiIcons.class);
+        for(AqiIcons icon : styleList){
+            Icon styleIcon = new Icon().withHref(icon.href);
+            Style style = doc.createAndAddStyle();
+            style.withId(icon.toString()).createAndSetIconStyle().withScale(1.0).withIcon(styleIcon);
+            style.createAndSetLabelStyle().withColor("00000000").withScale(1.0);
+            style.createAndSetBalloonStyle().withBgColor("ffffffff").withDisplayMode(DisplayMode.DEFAULT);
+        }
     }
 }
